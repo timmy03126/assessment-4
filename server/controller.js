c3a2815674e6dbc69ad086b84ae98db02e09648c
Timothy Childs <timmy03126@gmail.com>
2023-10-06 15:43:13 -0400

    initial commit

---
+++ server/controller.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    getCompliment: (req, res) => {
+        const compliments = ["Gee, you're a smart cookie!", "Cool shirt!", "Your Javascript skills are stellar."];
+      
+        // choose random compliment
+        let randomIndex = Math.floor(Math.random() * compliments.length);
+        let randomCompliment = compliments[randomIndex];
+      
+        res.status(200).send(randomCompliment);
+    },
+    getFortune: (req, res) => {
+        const fortunes = ["Bide your time, for success is near.", "Do not let ambitions overshadow small success.", "Do not underestimate yourself. Human beings have unlimited potentials.", "Allow compassion to guide your decisions.", "Happiness begins with facing life with a smile and a wink."]
+
+        let randomSpot = Math.floor(Math.random() * fortunes.length)
+        let randomFortune = fortunes[randomSpot]
+
+        res.status(200).send(randomFortune)
+    }
+
+}
